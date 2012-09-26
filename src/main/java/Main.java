79fe5b7758ed25c7d073e48dcff6c646a0b68cad
Irssildur <irssildur@gmail.com>
2012-09-26 15:06:33 +0200

    Added syso to tests and and an if statement to the create test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -38,45 +38,50 @@ public class Main {
 
     private static void listAllChildrenTest(ChildRepository repo) {
         for (Child child : repo.listAllChildren()) {
-            child.toString();
+            System.out.println("listAllChildrenTest: " + child.toString());
         }
     }
 
     private static void listSchoolClassTest(ChildRepository repo) {
         for (SchoolClass schoolClass : repo.listAllSchoolClass()) {
-            System.out.println(schoolClass.getChildren().toString());
+            System.out.println("listSchoolClassTest: " + schoolClass.getChildren().toString());
         }
     }
 
     private static void createSchoolClassTest(ChildRepository repo) {
-        SchoolClass class1 = new SchoolClass();
-        SchoolClass class2 = new SchoolClass();
         
-        List<Address> addr = new ArrayList<Address>();
-        addr.add(new Address("Budapest1", "Futo utca1"));
-        addr.add(new Address("Budapest2", "Futo utca1"));
-        addr.add(new Address("Budapest3", "Futo utca1"));
-        addr.add(new Address("Budapest4", "Futo utca1"));
-        addr.add(new Address("Budapest5", "Futo utca1"));
-        addr.add(new Address("Budapest6", "Futo utca1"));
-        addr.add(new Address("Budapest7", "Futo utca1"));
+        if (repo.listAllSchoolClass().size() == 0) {
         
-
-        List<Child> children1 = new ArrayList<Child>();
-        children1.add(new Child("Gyerek1", addr.get(0), class1));
-        children1.add(new Child("Gyerek2", addr.get(1), class1));
-        children1.add(new Child("Gyerek3", addr.get(2), class1));
-        children1.add(new Child("Gyerek4", addr.get(3), class1));
-        class1.setChildren(children1);
-        
-        List<Child> children2 = new ArrayList<Child>();
-        children2.add(new Child("Gyerek5", addr.get(4), class2));
-        children2.add(new Child("Gyerek6", addr.get(5), class2));
-        children2.add(new Child("Gyerek7", addr.get(6), class2));
-        class2.setChildren(children2);
-        
-        repo.createSchoolClass(class1);
-        repo.createSchoolClass(class2);
+            SchoolClass class1 = new SchoolClass();
+            SchoolClass class2 = new SchoolClass();
+            
+            List<Address> addr = new ArrayList<Address>();
+            addr.add(new Address("Budapest1", "Futo utca1"));
+            addr.add(new Address("Budapest2", "Futo utca1"));
+            addr.add(new Address("Budapest3", "Futo utca1"));
+            addr.add(new Address("Budapest4", "Futo utca1"));
+            addr.add(new Address("Budapest5", "Futo utca1"));
+            addr.add(new Address("Budapest6", "Futo utca1"));
+            addr.add(new Address("Budapest7", "Futo utca1"));
+            
+    
+            List<Child> children1 = new ArrayList<Child>();
+            children1.add(new Child("Gyerek1", addr.get(0), class1));
+            children1.add(new Child("Gyerek2", addr.get(1), class1));
+            children1.add(new Child("Gyerek3", addr.get(2), class1));
+            children1.add(new Child("Gyerek4", addr.get(3), class1));
+            class1.setChildren(children1);
+            
+            List<Child> children2 = new ArrayList<Child>();
+            children2.add(new Child("Gyerek5", addr.get(4), class2));
+            children2.add(new Child("Gyerek6", addr.get(5), class2));
+            children2.add(new Child("Gyerek7", addr.get(6), class2));
+            class2.setChildren(children2);
+            
+            repo.createSchoolClass(class1);
+            repo.createSchoolClass(class2);
+            
+        }
     }
 
 }
